5865af28617f0a0b1810e7aaf2f8519adf20c776
mrcool-59 <mrcoolmods@live.fr>
2026-08-12 23:34:03 +0200

    Fixed "include" that should have been "incbin"

--- disasm/layout/sf2-expanded-19.asm
+++ disasm/layout/sf2-expanded-19.asm
@@ -2,8 +2,8 @@
 ; GAME SECTION 19 : ROM EXPANSION.
 
 		if (EXPANDED_MUSIC_BANKS=1)
-			include "data/sound/musicbankext1.bin"							; Extra Music Banks 1 and 0
-			include "data/sound/musicbankext0.bin"
+			incbin "data/sound/musicbankext1.bin"							; Extra Music Banks 1 and 0
+			incbin "data/sound/musicbankext0.bin"
 		endif
 		
         include "data\graphics\maps\maptilesets\entries.asm"                ; Map Tilesets
